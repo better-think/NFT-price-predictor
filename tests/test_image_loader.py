f77fb638cf3817dcb1e9e7d12fed416d89d2d303
chaojin752 <chaojin752@gmail.com>
2021-12-09 07:21:56 +0300

    Split test data paths

--- tests/test_image_loader.py
+++ tests/test_image_loader.py
@@ -5,15 +5,17 @@ from image_loader import ImageLoader
 
 
 class TestImageLoader(TestCase):
+    __base_path = 'image-loader-data'
+
     def test_load_images(self):
-        images = ImageLoader(join('data', 'images')).load_images()
+        images = ImageLoader(join(self.__base_path, 'images')).load_images()
         [self.assert_image_has_3_channels(image_and_id.image) for image_and_id in images]
 
     def assert_image_has_3_channels(self, image):
         self.assertEquals(image.shape[2], 3)
 
     def test_find_png_files(self):
-        base_path = join('data', 'images')
+        base_path = join(self.__base_path, 'images')
         expected_files = [join(base_path, file_name) for file_name in [
             join('partition=0', '195000.png'),
             join('partition=0', '34359969000.png'),
